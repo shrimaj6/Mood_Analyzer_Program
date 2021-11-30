ec0c8656387691911c982db23053e66ead0eaaca
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-30 13:43:18 +0530

    Added UC-1 Program.

---
+++ Mood_Analyzer_Program/MoodAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyzer_Program
+{
+    public class MoodAnalyzer
+    {
+        public string msg;
+        public MoodAnalyzer(string msg)
+        {
+            this.msg = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            if (msg.ToLower().Contains("sad"))
+                return "SAD";
+
+            else
+                return "HAPPY";
+        }
+
+    }
+}
--- Mood_Analyzer_Test/UnitTest1.cs
+++ Mood_Analyzer_Test/UnitTest1.cs
@@ -1,13 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Mood_Analyzer_Program;
 namespace Mood_Analyzer_Test
+
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MsgSadMood()
         {
+            string msg = "I am in sad mood";
+            string expected = "SAD";
+
+            MoodAnalyzer mood = new MoodAnalyzer(msg);
+
+            string actual = mood.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void MsgHaapyMood()
+        {
+            string msg = "I am in Any mood";
+            string expected = "HAPPY";
+
+            MoodAnalyzer mood = new MoodAnalyzer(msg);
+
+            string actual = mood.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
