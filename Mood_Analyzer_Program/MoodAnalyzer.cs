f28e705ff4ea33d5ec649fad4465e49f254c4844
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-30 16:24:14 +0530

    Added UC-2 Program.

--- Mood_Analyzer_Program/MoodAnalyzer.cs
+++ Mood_Analyzer_Program/MoodAnalyzer.cs
@@ -7,18 +7,25 @@ namespace Mood_Analyzer_Program
     public class MoodAnalyzer
     {
         public string msg;
-        //public MoodAnalyzer(string msg)
-        //{
-        //    this.msg = msg;
-        //}
-
-        public string AnalyseMood(string msg)
+        public MoodAnalyzer(string msg)
         {
-            if (msg.ToLower().Contains("sad"))
-                return "SAD";
+            this.msg = msg;
+        }
 
-            else
-                return "HAPPY";
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (msg.ToLower().Contains(""))
+                    return "HAPPY";
+
+                else
+                    return "SAD";
+            }
+            catch (NullReferenceException msg)
+            {
+                return "Happy";
+            }
         }
 
     }
--- Mood_Analyzer_Test/UnitTest1.cs
+++ Mood_Analyzer_Test/UnitTest1.cs
@@ -7,28 +7,28 @@ namespace Mood_Analyzer_Test
     public class UnitTest1
     {
         [TestMethod]
-        public void MsgSadMood()
+        public void NullMood()
         {
-            string msg = "I am in sad mood";
-            string expected = "SAD";
-
-            MoodAnalyzer mood = new MoodAnalyzer();
-
-            string actual = mood.AnalyseMood(msg);
-
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void MsgHaapyMood()
-        {
-            string msg = "I am in Any mood";
+            string msg = " ";
             string expected = "HAPPY";
 
-            MoodAnalyzer mood = new MoodAnalyzer();
+            MoodAnalyzer mood = new MoodAnalyzer(msg);
 
-            string actual = mood.AnalyseMood(msg);
+            string actual = mood.AnalyseMood();
 
             Assert.AreEqual(expected, actual);
         }
+         // [TestMethod]
+        //public void MsgHaapyMood()
+        //{
+        //    string msg = "I am in Any mood";
+        //    string expected = "HAPPY";
+
+        //    MoodAnalyzer mood = new MoodAnalyzer();
+
+        //    string actual = mood.AnalyseMood(msg);
+
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
