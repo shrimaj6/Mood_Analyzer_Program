d4a40a2fd9e0d03091b9622746704c898f76c280
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-30 12:38:36 +0530

    Add project files.

---
+++ Mood_Analyzer_Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mood_Analyzer_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Program");
+        }
+    }
+}
---
+++ Mood_Analyzer_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mood_Analyzer_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
